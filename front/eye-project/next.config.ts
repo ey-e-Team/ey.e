cd03ffd301eefc1885fc7903a88a44aded46db9c
Guilherme1-jpg <henriqueoliveiragui18@gmail.com>
2026-03-18 09:13:43 -0300

    Config para deploy

--- front/eye-project/next.config.ts
+++ front/eye-project/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/ey.e',
+  assetPrefix: '/ey.e/',
 };
 
 export default nextConfig;
